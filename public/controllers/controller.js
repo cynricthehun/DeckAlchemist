e42d817e1c0ba2fa2f180e8145bdb23b8e37a4df
Joey Meyer <joeymeyer1986@gmail.com>
2017-11-06 16:12:39 -0600

    removed the success check which wasn't supported anymore.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,7 +7,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
     name: "",
     email: "",
     number: "",
-    admin: ""
+    admin: false
   }
   var refresh = function() {
     $http.get('/userList').then(successCallback, errorCallback);
@@ -15,20 +15,27 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
     function successCallback(response){
       console.log('I got the data I requested');
       $scope.userList = response.data;
-      $scope.user = "";
+      $scope.newUser = {
+        name: "",
+        email: "",
+        number: "",
+        admin: false
+      }
       console.log(response.data);
     }
     function errorCallback(error){
         //error code
     }
   }
+
   refresh();
+
   $scope.addUser = function() {
-    console.log($scope.user)
-    $http.post('/userList', $scope.newUser).success(function(response){
+    console.log($scope.newUser)
+    $http.post('/userList', $scope.newUser).then(function(response){
       console.log(response);
+      refresh();
     });
-    refresh();
   };
 
 }]);
