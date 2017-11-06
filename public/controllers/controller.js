884eee900e794da071090906b60e7e44e7830fef
Joey Meyer <joeymeyer1986@gmail.com>
2017-11-05 23:50:29 -0600

    added mongodb, added gitignore

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,17 +2,25 @@ var myApp = angular.module('DeckAlchemist', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
   console.log("Hello World from controller");
 
-$scope.userList = [];
+  $scope.userList = [];
+  var refresh = function() {
+    $http.get('/userList').then(successCallback, errorCallback);
 
-  $http.get('/userList').then(successCallback, errorCallback);
-
-  function successCallback(response){
-    console.log('I got the data I requested');
-    $scope.userList = response.data;
-    console.log(response.data);
+    function successCallback(response){
+      console.log('I got the data I requested');
+      $scope.userList = response.data;
+      $scope.user = "";
+      console.log(response.data);
+    }
+    function errorCallback(error){
+        //error code
+    }
   }
-  function errorCallback(error){
-      //error code
-  }
+  refresh();
+  $scope.addUser = function() {
+    console.log($scope.user)
+    $http.post('/userList', $scope.user);
+    refresh();
+  };
 
 }]);
--- server.js
+++ server.js
@@ -1,32 +1,25 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('deck_alchemist_development', ['deck_alchemist_development']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/userList', function (req, res) {
   console.log("I received a GET request")
+  db.deck_alchemist_development.find(function(err, docs){
+    console.log(docs);
+    res.json(docs);
+  });
+});
 
-  person1 = {
-    name: 'Tim',
-    email: 'Tim@email.com',
-    number: '(111) 111-1111',
-    admin: true
-  };
-  person2 = {
-    name: 'Joey',
-    email: 'Joey@email.com',
-    number: '(111) 111-1111',
-    admin: true
-  };
-  person3 = {
-    name: 'John',
-    email: 'John@email.com',
-    number: '(111) 111-1111',
-    admin: true
-  };
-
-  var userList = [person1, person2, person3];
-  res.json(userList);
+app.post('/userList', function (req, res) {
+  console.log(req.body);
+  db.userList.insert(req.body, function(err, doc) {
+    res.json(doc);
+  })
 });
 
 app.listen(3000);
