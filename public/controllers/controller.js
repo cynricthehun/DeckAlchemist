220ea674b9f888a60372f9909478dbb5c68530c4
Joey Meyer <joeymeyer1986@gmail.com>
2017-11-05 20:31:44 -0600

    updated the controller to narrow the response down to the data element.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,13 +2,17 @@ var myApp = angular.module('DeckAlchemist', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
   console.log("Hello World from controller");
 
+$scope.userList = [];
+
   $http.get('/userList').then(successCallback, errorCallback);
 
   function successCallback(response){
     console.log('I got the data I requested');
-    $scope.userList = response;
+    $scope.userList = response.data;
+    console.log(response.data);
   }
   function errorCallback(error){
       //error code
   }
+
 }]);
