255c9cd7df601aedec37016cc0e545f7f3b05b00
Joey Meyer <joeymeyer1986@gmail.com>
2017-11-12 21:51:17 -0600

    started model for postgres database. added a start_service.exe file to run mongoDB and in the future to begin other services required to start the application.

---
+++ services/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <direct.h>
+
+using namespace std;
+
+string current_working_directory()
+{
+    char* cwd = _getcwd( 0, 0 ) ; // **** microsoft specific ****
+    std::string working_directory(cwd) ;
+    std::free(cwd) ;
+    return working_directory ;
+}
+
+int main(){
+  cout << "finding your mongoDB database folder.";
+  cout << current_working_directory() << endl;
+  string path = current_working_directory();
+  string dataPath = "/data/db";
+  string fullPath = string(path) + string(dataPath);
+  string command = "mongod -dbpath ";
+  std::string result = std::string(command) + std::string(fullPath);
+
+  if (fullPath.find("/data/db") == true){
+      cout << result << endl;
+  }
+  else{
+      cout << "No Database Found." << endl;
+  }
+
+  cout << "Starting MongoDB" << endl;
+  system(result.c_str());
+  cin.get();
+  return 0;
+}
